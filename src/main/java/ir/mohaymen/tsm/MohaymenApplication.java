6ea4c8d9b6943accd937e726a3838011cc3a4104
Dev-AliGhasemi <ali.ghasemeey@gmail.com>
2025-01-13 17:41:22 +0330

    Initial Commit.

---
+++ src/main/java/ir/mohaymen/tsm/MohaymenApplication.java
@@ -0,0 +1,13 @@
+package ir.mohaymen.tsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohaymenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohaymenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/mohaymen/tsm/MohaymenApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mohaymen.tsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohaymenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
